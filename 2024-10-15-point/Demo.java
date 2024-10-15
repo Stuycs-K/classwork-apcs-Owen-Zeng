22af80f6e62e3241f44b8629fcf62c421dcfbba3
Owen Zeng <owenz20@nycstudnets.net>
2024-10-15 11:52:18 -0400

    added test cases and started verifying, not done yet

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -13,11 +13,21 @@ public class Demo{
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
     // testing new Point constructor
-    Point p4 = new Point(p3);
+    Point p4 = new Point(0,0);
+    Point p5 = new Point(p4);
+
     System.out.println( p3);
     System.out.println(p4);
+    System.out.println(p5);
+
     System.out.println( distance(p1,p2));
+    System.out.println( distance(p3,p2)); //
+    System.out.println( )
+
     System.out.println( Point.distance(p1,p2));
+    System.out.println( Point.distance(p5 ,p1));
+
     System.out.println( p1.distanceTo(p2));
+    System.out.println( p4.distanceTo(p1));
   }
 }
