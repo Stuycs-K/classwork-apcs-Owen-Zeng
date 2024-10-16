c501ed3f506dd9a1af0fcd2daff3471a37cd73e8
Owen-Zeng <owenzeng1002@gmail.com>
2024-10-16 00:18:26 -0400

    added some test cases and verified them

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -14,20 +14,19 @@ public class Demo{
     Point p3 = new Point(3,4);
     // testing new Point constructor
     Point p4 = new Point(0,0);
-    Point p5 = new Point(p4);
+    Point p5 = new Point(9,7);
 
     System.out.println( p3);
     System.out.println(p4);
     System.out.println(p5);
 
     System.out.println( distance(p1,p2));
-    System.out.println( distance(p3,p2)); //
-    System.out.println( )
+    System.out.println( distance(p3,p4)); // 5
 
     System.out.println( Point.distance(p1,p2));
-    System.out.println( Point.distance(p5 ,p1));
+    System.out.println( Point.distance(p5 ,p1)); // 10
 
     System.out.println( p1.distanceTo(p2));
-    System.out.println( p4.distanceTo(p1));
+    System.out.println( p4.distanceTo(p1)); // root 2
   }
 }
