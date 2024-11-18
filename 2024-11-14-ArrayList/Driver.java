8703515cd8789f0744fb52041907a8f4fede64e8
Owen-Zeng <owenzeng1002@gmail.com>
2024-11-17 23:43:39 -0500

    fixed indexing error in makeReversedList and made tests.

--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+
 public class Driver {
   public static void main(String[] args){
-    System.out.println(ArrayListPractice.createRandomArray(20));
+    ArrayList<String> rand = new ArrayList<>();
+
+    rand = ArrayListPractice.createRandomArray(10);
+    // rand = ArrayListPractice.createRandomArray(200000);
+    System.out.println(rand);
+    ArrayListPractice.replaceEmpty(rand);
+    System.out.println(rand);
+
+    ArrayList<String> rand1 = new ArrayList<>();
+
+    rand1 = ArrayListPractice.createRandomArray(10);
+    // rand1 = ArrayListPractice.createRandomArray(100000);
+    System.out.println(rand1);
+    System.out.println(ArrayListPractice.makeReversedList(rand1));
+
+    System.out.println(ArrayListPractice.mixLists(rand, rand1));
+
+
+
   }
 }
