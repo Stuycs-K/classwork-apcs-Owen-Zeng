21074c1943e0c6ff3a7bece5da6c7efec8908887
Owen-Zeng <owenzeng1002@gmail.com>
2024-11-18 22:41:07 -0500

    fixed errors and tested smaller testcases, larger testcases dont work

---
+++ 2024-11-18-advent2016/Day1.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Day1{
+
+
+  public static int distance(){
+    int vertical = 0;
+    int horizontal = 0;
+    int rotation = 0;
+
+    try {
+      File file = new File("input.txt");//1
+      Scanner input = new Scanner(file);
+
+        while(input.hasNext()){
+          String nextDir = input.next();
+          if(nextDir.contains("R")){
+            rotation += 90;
+          }
+          else{
+            rotation -= 90;
+          }
+
+          if(rotation == 360){
+            rotation = 0;
+          }
+          if(rotation == -90){
+            rotation = 270;
+          }
+
+
+          if(rotation == 0){
+            vertical += (Integer.parseInt(nextDir.substring(1,2)));
+          }
+          if(rotation == 90){
+            horizontal += (Integer.parseInt(nextDir.substring(1,2)));
+          }
+          if(rotation == 180){
+            vertical -= (Integer.parseInt(nextDir.substring(1,2)));
+          }
+          if(rotation == 270){
+            horizontal -= (Integer.parseInt(nextDir.substring(1,2)));
+
+          }
+        }
+        input.close();
+    } catch (FileNotFoundException ex) {
+      //File not found what should you do?
+      System.out.println("File not found");
+      return Math.abs(vertical) + Math.abs(horizontal); //you can return from a void function just don't put a value.
+    }
+    System.out.println(vertical + " " + horizontal);
+    return Math.abs(vertical) + Math.abs(horizontal);
+    
+  }
+
+}
