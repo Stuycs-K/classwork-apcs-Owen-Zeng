af11c58c51da501b6840cdbe435614360f17dff6
Owen-Zeng <owenzeng1002@gmail.com>
2025-01-05 23:43:03 -0500

    finished no repeating letters makeWords.

--- 2025-01-03/Words.java
+++ 2025-01-03/Words.java
@@ -1,19 +1,30 @@
+import java.util.ArrayList;
+
 public class Words{
 
   public static void main(String[] args){
-
+    ArrayList<String> arr = new ArrayList<String>();
+    System.out.println(makeWords(3, "", "89", arr));
   }
 
-  public static void makeWords(int remainingLetters, String result , String alphabet){
-    if(remainingLetters == 1){
-      for(int i = 0; i < alphabet.length; i++){
-        System.out.println(result + alphabet.charAt(i));
+  public static ArrayList<String>  makeWords(int remainingLetters, String result , String alphabet, ArrayList<String> words){
+    if(remainingLetters == 0){
+        words.add(result);
+    }
+
+
+    else{
+      for(int i =0; i < alphabet.length(); i++){
+        if(result.length() > 0){
+          if(result.charAt(result.length()- 1) != alphabet.charAt(i)){
+            makeWords(remainingLetters-1, result + alphabet.charAt(i), alphabet, words);
+          }
+        }
+        else{
+          makeWords(remainingLetters-1, result + alphabet.charAt(i), alphabet, words);
+        }
       }
     }
-
-    for(int i =0; i < remainingLetters; i++){
-      makeWords(remainingLetters-1, result + letter, alphabet)
-
-    }
+    return words;
   }
 }
